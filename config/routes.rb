8e8e1ad165ff0211b4502d055cb1561fa0495e4d
Teppei Nakano <keiohigh2nd@Teppei-no-MacBook-Air.local>
2016-10-21 17:00:24 +0900

    article function

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:index, :show, :new, :edit, :create, :update] do
     resources :notes
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161011093215) do
+ActiveRecord::Schema.define(version: 20161021074136) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "notes", force: :cascade do |t|
     t.integer  "user_id"
